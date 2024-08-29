748fc4b4a84226686f4004583bfba199ef0c3630
Emad Almahdi <emad.almahdi@robursafe.com>
2024-08-29 13:29:02 +0200

    ..

--- Src/Enum/SettingType.php
+++ Src/Enum/SettingType.php
@@ -1,13 +1,34 @@
 <?php declare(strict_types=1);
 
 namespace Temant\SettingsManager\Enum {
-
+    /**
+     * Enum SettingType defines the possible types for a setting in the settings manager.
+     */
     enum SettingType: string
     {
+        /**
+         * Represents a string type setting.
+         */
         case STRING = 'string';
+
+        /**
+         * Represents an integer type setting.
+         */
         case INTEGER = 'integer';
+
+        /**
+         * Represents a boolean type setting.
+         */
         case BOOLEAN = 'boolean';
+
+        /**
+         * Represents a float type setting.
+         */
         case FLOAT = 'float';
+
+        /**
+         * Represents a JSON type setting.
+         */
         case JSON = 'json';
     }
 }
